996c5bef2f3d86e6cc69e2fe0dc70366a7786f1f
EverNorif <1320346985@qq.com>
2025-08-06 10:51:10 +0800

    so101 biarm triplecam finetune support.

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -245,6 +245,18 @@ class So101SingleCamDataConfig(So100DataConfig):
 ###########################################################################################
 
 
+class So101BiArmTripleCamDataConfig(So100DataConfig):
+    video_keys = ["video.top", "video.left", "video.right"]
+    state_keys = ["state.left_arm", "state.left_gripper", "state.right_arm", "state.right_gripper"]
+    action_keys = ["action.left_arm", "action.left_gripper", "action.right_arm", "action.right_gripper"]
+    language_keys = ["annotation.human.task_description"]
+    observation_indices = [0]
+    action_indices = list(range(16))
+
+
+###########################################################################################
+
+
 class UnitreeG1DataConfig(BaseDataConfig):
     video_keys = ["video.rs_view"]
     state_keys = ["state.left_arm", "state.right_arm", "state.left_hand", "state.right_hand"]
@@ -901,6 +913,7 @@ DATA_CONFIG_MAP = {
     "so100": So100DataConfig(),
     "so100_dualcam": So100DualCamDataConfig(),
     "so101_singlecam": So101SingleCamDataConfig(),
+    "so101_biarm_triplecam": So101BiArmTripleCamDataConfig(),
     "unitree_g1": UnitreeG1DataConfig(),
     "unitree_g1_full_body": UnitreeG1FullBodyDataConfig(),
     "oxe_droid": OxeDroidDataConfig(),
